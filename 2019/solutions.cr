710124cf9d41218b1f07e3c9a72b4e41df28a329
Phil Nash <philnash@gmail.com>
2019-12-18 00:15:24 +1100

    Update readmes

--- 2019/solutions.cr
+++ 2019/solutions.cr
@@ -1,8 +1,16 @@
 require "./days/day1"
+require "./days/day2"
 
 puts "--- Day 1: The Tyranny of the Rocket Equation ---"
-input = File.read_lines("./day1.txt")
+input = File.read_lines("./days/day1.txt")
 total_fuel = input.map { |mass_str| fuel_required(mass_str.to_i) }.sum
 puts "Total fuel required is #{total_fuel}"
 total_fuel = input.map { |mass_str| total_module_fuel_required(mass_str.to_i) }.sum
 puts "Total fuel required including fuel for fuel is #{total_fuel}"
+
+puts "--- Day 2: 1202 Program Alarm ---"
+input = File.read("./days/day2.txt")
+computer = Intcode.new(input).initialize_memory(12,2)
+puts "Position 0 is #{computer.run.memory.first}"
+puts "Noun and verb are: #{noun_and_verb_for_result(computer, 19690720)}"
+
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ These are my solutions for the [Advent of Code](https://adventofcode.com/).
 
 ## Years
 
+* [2019](./2019)
 * [2018](./2018)
 * [2017](./2017)
