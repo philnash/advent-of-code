058f75249becf7e32d422a80517ddf9580fa5ecf
Phil Nash <philnash@gmail.com>
2019-12-18 00:13:26 +1100

    Day 2 solution

---
+++ 2019/days/day2.cr
@@ -0,0 +1,58 @@
+class Intcode
+  getter memory : Array(Int32)
+  def initialize(@program : String)
+    @memory = @program.split(',').map { |s| s.to_i }
+  end
+
+  def initialize_memory(noun = 0, verb = 0)
+    @memory = @program.split(',').map { |s| s.to_i }
+    @memory[1] = noun
+    @memory[2] = verb
+    self
+  end
+
+  def run
+    pointer = 0
+    opcode = @memory[pointer]
+    while opcode != 99
+      input_position1 = @memory[pointer+1]
+      input_position2 = @memory[pointer+2]
+      output_position = @memory[pointer+3]
+      input1 = @memory[input_position1]
+      input2 = @memory[input_position2]
+      if opcode == 1
+        output = input1 + input2
+      elsif opcode == 2
+        output = input1 * input2
+      else
+        raise "Incorrect opcode"
+      end
+      @memory[output_position] = output
+      pointer += 4
+      opcode = @memory[pointer]
+    end
+    self
+  end
+
+  def to_s
+    @memory.join(",")
+  end
+end
+
+def noun_and_verb_for_result(computer, result)
+  noun = 0
+  verb = 0
+  solved = false
+  while noun < 100
+    while verb < 100
+      computer.initialize_memory(noun, verb)
+      solved = computer.run.memory.first == result
+      break if solved
+      verb += 1
+    end
+    break if solved
+    verb = 0
+    noun += 1
+  end
+  return "#{noun}#{verb}"
+end
---
+++ 2019/spec/day2_spec.cr
@@ -0,0 +1,29 @@
+require "spec"
+require "../days/day2"
+
+describe Intcode do
+  it "turns 1,0,0,0,99 into 2,0,0,0,99" do
+    computer = Intcode.new("1,0,0,0,99")
+    computer.run.to_s.should eq("2,0,0,0,99")
+  end
+
+  it "turns 2,3,0,3,99 into 2,3,0,6,99" do
+    computer = Intcode.new("2,3,0,3,99")
+    computer.run.to_s.should eq("2,3,0,6,99")
+  end
+
+  it "turns 2,4,4,5,99,0 into 2,4,4,5,99,9801" do
+    computer = Intcode.new("2,4,4,5,99,0")
+    computer.run.to_s.should eq("2,4,4,5,99,9801")
+  end
+
+  it "turns 1,1,1,4,99,5,6,0,99 into 30,1,1,4,2,5,6,0,99" do
+    computer = Intcode.new("1,1,1,4,99,5,6,0,99")
+    computer.run.to_s.should eq("30,1,1,4,2,5,6,0,99")
+  end
+
+  it "turns 1,9,10,3,2,3,11,0,99,30,40,50 into 3500,9,10,70,2,3,11,0,99,30,40,50" do
+    computer = Intcode.new("1,9,10,3,2,3,11,0,99,30,40,50")
+    computer.run.to_s.should eq("3500,9,10,70,2,3,11,0,99,30,40,50")
+  end
+end
