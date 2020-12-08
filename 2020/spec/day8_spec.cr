f8dbdf739fd9440ea1834eb5b53860001e4fbf82
Phil Nash <philnash@gmail.com>
2020-12-08 17:00:15 +1100

    Adds tests for day 8

--- 2020/spec/day8_spec.cr
+++ 2020/spec/day8_spec.cr
@@ -1,2 +1,14 @@
 require "spec"
 require "../days/day8.cr"
+
+input = ["nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"]
+
+describe Console do
+  it "returns the accumulator and whether the program finished correctly" do
+    Console.new.process_instructions(input).should eq({5, false})
+  end
+
+  it "returns the accumulator when the program finishes correctly" do
+    Console.new.find_incorrect_instruction(input).should eq(8)
+  end
+end
