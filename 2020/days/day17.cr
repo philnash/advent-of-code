9faae6c47e6e00ce2298154dd3bfaf014b9834ab
Phil Nash <philnash@gmail.com>
2020-12-17 21:13:31 +1100

    Remove test data

--- 2020/days/day17.cr
+++ 2020/days/day17.cr
@@ -54,15 +54,15 @@ class ConwayCubes
 end
 
 class ConwayCubes4D
-  getter layers : Array(Array(Array(Array(Bool))))
+  getter dimensions : Array(Array(Array(Array(Bool))))
 
   def initialize(input : String)
-    @layers = [[input.split("\n").map { |line| line.split("").map { |cube| cube == "#" } }]]
+    @dimensions = [[input.split("\n").map { |line| line.split("").map { |cube| cube == "#" } }]]
   end
 
   def cycle
-    new_width = @layers.first.first.size + 2
-    @layers.each { |dim|
+    new_width = @dimensions.first.first.size + 2
+    @dimensions.each { |dim|
       dim.each { |layer|
         layer.each { |line|
           line.unshift(false).push(false)
@@ -81,7 +81,7 @@ class ConwayCubes4D
         )
       )
     }
-    @layers.push(
+    @dimensions.push(
       Array(Array(Array(Bool))).new(new_width,
         Array(Array(Bool)).new(new_width,
           Array(Bool).new(new_width, false)
@@ -96,24 +96,24 @@ class ConwayCubes4D
     )
 
     coords = (-1..1).flat_map { |w| (-1..1).flat_map { |z| (-1..1).flat_map { |y| (-1..1).map { |x| [x, y, z, w] } } } }.reject { |coords| coords == [0, 0, 0, 0] }
-    @layers = @layers.map_with_index { |dim, w|
+    @dimensions = @dimensions.map_with_index { |dim, w|
       dim.map_with_index { |layer, z|
         layer.map_with_index { |line, y|
           line.map_with_index { |cube, x|
             active_neighbours = coords.map { |coord|
               begin
-                @layers[w + coord[3]][z + coord[2]][y + coord[1]][x + coord[0]]
+                @dimensions[w + coord[3]][z + coord[2]][y + coord[1]][x + coord[0]]
               rescue
                 false
               end
             }
             active_neighbours = active_neighbours.count(true)
-            if @layers[w][z][y][x] && !(active_neighbours == 2 || active_neighbours == 3)
+            if @dimensions[w][z][y][x] && !(active_neighbours == 2 || active_neighbours == 3)
               false
-            elsif !@layers[w][z][y][x] && active_neighbours == 3
+            elsif !@dimensions[w][z][y][x] && active_neighbours == 3
               true
             else
-              @layers[w][z][y][x]
+              @dimensions[w][z][y][x]
             end
           }
         }
@@ -123,14 +123,6 @@ class ConwayCubes4D
   end
 
   def count
-    @layers.flat_map { |dim| dim.flat_map { |layer| layer.flat_map { |line| line.flat_map { |cube| cube ? 1 : 0 } } } }.sum
+    @dimensions.flat_map { |dim| dim.flat_map { |layer| layer.flat_map { |line| line.flat_map { |cube| cube ? 1 : 0 } } } }.sum
   end
 end
-
-input = ".#.
-..#
-###"
-
-cubes = ConwayCubes4D.new(input)
-6.times { cubes.cycle }
-puts cubes.count
