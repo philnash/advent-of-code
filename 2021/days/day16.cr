a471409405f18b84506bc3e5a0fa2e1be84983d4
Phil Nash <philnash@gmail.com>
2021-12-30 20:52:16 +1200

    Day 16 2021

---
+++ 2021/days/day16.cr
@@ -0,0 +1,159 @@
+module Packets
+  abstract class Base
+    property version : Int32
+    property type_id : Int32
+
+    def initialize(@version, @type_id)
+    end
+
+    abstract def version_total
+    abstract def value
+  end
+
+  class Literal < Base
+    property value : Int64
+
+    def initialize(@version, @type_id, @value)
+      super(@version, @type_id)
+    end
+
+    def version_total
+      @version
+    end
+  end
+
+  class Operator < Base
+    property sub_packets : Array(Packets::Base)
+
+    def initialize(@version, @type_id, @sub_packets)
+      super(@version, @type_id)
+    end
+
+    def version_total
+      @version + sub_packets.map(&.version_total).sum
+    end
+
+    def value
+      case type_id
+      when 0
+        sub_packets.map(&.value).sum
+      when 1
+        sub_packets.map(&.value).product
+      when 2
+        sub_packets.map(&.value).min
+      when 3
+        sub_packets.map(&.value).max
+      when 5
+        sub_packets[0].value > sub_packets[1].value ? 1_i64 : 0_i64
+      when 6
+        sub_packets[0].value < sub_packets[1].value ? 1_i64 : 0_i64
+      when 7
+        sub_packets[0].value == sub_packets[1].value ? 1_i64 : 0_i64
+      else
+        0_i64
+      end
+    end
+  end
+
+  MAPPING = {
+    "0" => "0000",
+    "1" => "0001",
+    "2" => "0010",
+    "3" => "0011",
+    "4" => "0100",
+    "5" => "0101",
+    "6" => "0110",
+    "7" => "0111",
+    "8" => "1000",
+    "9" => "1001",
+    "A" => "1010",
+    "B" => "1011",
+    "C" => "1100",
+    "D" => "1101",
+    "E" => "1110",
+    "F" => "1111",
+  }
+
+  def self.parse(input : String)
+    binary = parse_hex(input)
+    parse_binary(binary)
+  end
+
+  def self.parse_binary(binary : String)
+    pointer = 0
+    packets = [] of Base
+    while pointer < binary.size
+      begin
+        headers = parse_headers(binary[pointer...(pointer + 6)])
+        pointer += 6
+        if headers[:type_id] == 4
+          number = binary[(pointer + 1)..(pointer + 4)]
+          while binary[pointer] != '0'
+            pointer += 5
+            number += binary[(pointer + 1)..(pointer + 4)]
+          end
+          pointer += 5
+          packets.push(Literal.new(headers[:version], headers[:type_id], number.to_i64(2)))
+        else
+          length_type_id = binary[pointer]
+          pointer += 1
+          if length_type_id == '0'
+            total_length = binary[pointer...pointer + 15].to_i(2)
+            pointer = pointer += 15
+            sub_packets = parse_binary(binary[pointer...pointer + total_length])
+            packets.push(Operator.new(headers[:version], headers[:type_id], sub_packets))
+            pointer += total_length
+          else
+            total_sub_packets = binary[pointer...pointer + 11].to_i(2)
+            pointer += 11
+            next_packets = parse_binary(binary[pointer..-1])
+            pointer = binary.size
+            packets.push(Operator.new(headers[:version], headers[:type_id], next_packets.shift(total_sub_packets)))
+            packets = packets + next_packets
+          end
+        end
+      rescue ex
+        return packets
+      end
+    end
+    packets
+  end
+
+  def self.parse_hex(input : String)
+    input.split("").map { |c| MAPPING[c] }.join("")
+  end
+
+  def self.parse_headers(input : String)
+    version = input[0..2].to_i(2)
+    type_id = input[3..5].to_i(2)
+    return {version: version, type_id: type_id}
+  end
+
+  def self.version_total(packets : Array(Base))
+    packets.map(&.version_total).sum
+  end
+end
+
+# first = Packets.parse("D2FE28")
+# puts first
+# second = Packets.parse("38006F45291200")
+
+# puts second
+# puts Packets.parse_binary("110100010100101001000100100")
+
+# third = Packets.parse("EE00D40C823060")
+# puts third
+
+puts Packets.version_total(Packets.parse("8A004A801A8002F478"))
+puts Packets.version_total(Packets.parse("620080001611562C8802118E34"))
+puts Packets.version_total(Packets.parse("C0015000016115A2E0802F182340"))
+puts Packets.version_total(Packets.parse("A0016C880162017C3686B18A3D4780"))
+
+puts Packets.parse("C200B40A82").map(&.value)
+puts Packets.parse("04005AC33890").map(&.value)
+puts Packets.parse("880086C3E88112").map(&.value)
+puts Packets.parse("CE00C43D881120").map(&.value)
+puts Packets.parse("D8005AC2A8F0").map(&.value)
+puts Packets.parse("F600BC2D8F").map(&.value)
+puts Packets.parse("9C005AC2F8F0").map(&.value)
+puts Packets.parse("9C0141080250320F1802104A08").map(&.value)
--- 2021/solutions.cr
+++ 2021/solutions.cr
@@ -1,94 +1,100 @@
 require "./days/*"
 
-puts "--- Day 1: Sonar Sweep ---"
-input = File.read_lines("./days/day1.txt").map(&.to_i)
-sweep = SonarSweep.new(input)
-puts "The number of depth increases is: #{sweep.count_increases}"
-puts "The number of depth increases by groups is: #{sweep.count_window_increases}"
+# puts "--- Day 1: Sonar Sweep ---"
+# input = File.read_lines("./days/day1.txt").map(&.to_i)
+# sweep = SonarSweep.new(input)
+# puts "The number of depth increases is: #{sweep.count_increases}"
+# puts "The number of depth increases by groups is: #{sweep.count_window_increases}"
 
-puts "--- Day 2: Dive! ---"
-input = File.read_lines("./days/day2.txt")
-sub = Dive.new
-sub.apply_instructions(input)
-puts "The position result it: #{sub.horizontal * sub.depth}"
-sub = DiveWithAim.new
-sub.apply_instructions(input)
-puts "The position result it: #{sub.horizontal * sub.depth}"
+# puts "--- Day 2: Dive! ---"
+# input = File.read_lines("./days/day2.txt")
+# sub = Dive.new
+# sub.apply_instructions(input)
+# puts "The position result it: #{sub.horizontal * sub.depth}"
+# sub = DiveWithAim.new
+# sub.apply_instructions(input)
+# puts "The position result it: #{sub.horizontal * sub.depth}"
 
-puts "--- Day 3: Binary Diagnostic ---"
-input = File.read_lines("./days/day3.txt")
-pc = PowerConsumption.new(input)
-puts "The combined gamma and epsilon rates are: #{pc.rates}"
-puts "The life support rating is #{pc.life_support_rating}"
+# puts "--- Day 3: Binary Diagnostic ---"
+# input = File.read_lines("./days/day3.txt")
+# pc = PowerConsumption.new(input)
+# puts "The combined gamma and epsilon rates are: #{pc.rates}"
+# puts "The life support rating is #{pc.life_support_rating}"
 
-puts "--- Day 4: Giant Squid ---"
-input = File.read("./days/day4.txt")
-game = Bingo::Game.new(input)
-puts "The victorious game score is: #{game.play}"
-puts "The last board to win score is: #{game.play_until_last_board_wins}"
+# puts "--- Day 4: Giant Squid ---"
+# input = File.read("./days/day4.txt")
+# game = Bingo::Game.new(input)
+# puts "The victorious game score is: #{game.play}"
+# puts "The last board to win score is: #{game.play_until_last_board_wins}"
 
-puts "--- Day 5: Hydrothermal Venture ---"
-input = File.read_lines("./days/day5.txt")
-vents = Hydrothermal::Vents.new(input)
-puts "The number of overlaps of verticals and horizontals is: #{vents.count_overlaps}"
-vents = Hydrothermal::Vents.new(input)
-puts "The number of overlaps of all lines is: #{vents.count_overlaps(true)}"
+# puts "--- Day 5: Hydrothermal Venture ---"
+# input = File.read_lines("./days/day5.txt")
+# vents = Hydrothermal::Vents.new(input)
+# puts "The number of overlaps of verticals and horizontals is: #{vents.count_overlaps}"
+# vents = Hydrothermal::Vents.new(input)
+# puts "The number of overlaps of all lines is: #{vents.count_overlaps(true)}"
 
-puts "--- Day 6: Lanternfish ---"
-input = File.read("./days/day6.txt")
-puts "The number of Lanternfish after 80 days is: #{School.new(input).add_days(80)}"
-puts "The number of Lanternfish after 256 days is: #{School.new(input).add_days(256)}"
+# puts "--- Day 6: Lanternfish ---"
+# input = File.read("./days/day6.txt")
+# puts "The number of Lanternfish after 80 days is: #{School.new(input).add_days(80)}"
+# puts "The number of Lanternfish after 256 days is: #{School.new(input).add_days(256)}"
 
-puts "--- Day 7: The Treachery of Whales ---"
-input = File.read("./days/day7.txt")
-puts "The fuel the crabs must use at a constant rate is: #{CrabSubmarines::ConstantFuelCost.new(input).fuel}"
-puts "The fuel the crabs must use at an increasing rate is: #{CrabSubmarines::IncreasingFuelCost.new(input).fuel}"
+# puts "--- Day 7: The Treachery of Whales ---"
+# input = File.read("./days/day7.txt")
+# puts "The fuel the crabs must use at a constant rate is: #{CrabSubmarines::ConstantFuelCost.new(input).fuel}"
+# puts "The fuel the crabs must use at an increasing rate is: #{CrabSubmarines::IncreasingFuelCost.new(input).fuel}"
 
-puts "--- Day 8: Seven Segment Search ---"
-input = File.read_lines("./days/day8.txt")
-puts "The number of ones, fours, sevens and eights is: #{SegmentSearch.unique_output_segment_count(input)}"
-puts "The combined total of the outputs is: #{SegmentSearch.sum_outputs(input)}"
+# puts "--- Day 8: Seven Segment Search ---"
+# input = File.read_lines("./days/day8.txt")
+# puts "The number of ones, fours, sevens and eights is: #{SegmentSearch.unique_output_segment_count(input)}"
+# puts "The combined total of the outputs is: #{SegmentSearch.sum_outputs(input)}"
 
-puts "--- Day 9: Smoke Basin ---"
-input = File.read_lines("./days/day9.txt")
-height_map = HeightMap.new(input)
-puts "The total risk level is: #{height_map.risk_level}"
-puts "The product of the largest three basins is: #{height_map.top_three_basins_product}"
+# puts "--- Day 9: Smoke Basin ---"
+# input = File.read_lines("./days/day9.txt")
+# height_map = HeightMap.new(input)
+# puts "The total risk level is: #{height_map.risk_level}"
+# puts "The product of the largest three basins is: #{height_map.top_three_basins_product}"
 
-puts "--- Day 10: Syntax Scoring ---"
-input = File.read_lines("./days/day10.txt")
-puts "The total score for syntax errors is: #{SyntaxChecker.score_errors(input)}"
-puts "The middle score for the completed rows is: #{SyntaxChecker.score_completions(input)}"
+# puts "--- Day 10: Syntax Scoring ---"
+# input = File.read_lines("./days/day10.txt")
+# puts "The total score for syntax errors is: #{SyntaxChecker.score_errors(input)}"
+# puts "The middle score for the completed rows is: #{SyntaxChecker.score_completions(input)}"
 
-puts "--- Day 11: Dumbo Octopus ---"
-input = File.read_lines("./days/day11.txt").map { |row| row.split("").map(&.to_i) }
-grid = OctopusGrid.new(input)
-puts "The number of octopus flashes after 100 steps is: #{grid.step(100)}"
-puts "The number of steps it takes to all flash at the same time is: #{100 + grid.step_until_synchronised}"
+# puts "--- Day 11: Dumbo Octopus ---"
+# input = File.read_lines("./days/day11.txt").map { |row| row.split("").map(&.to_i) }
+# grid = OctopusGrid.new(input)
+# puts "The number of octopus flashes after 100 steps is: #{grid.step(100)}"
+# puts "The number of steps it takes to all flash at the same time is: #{100 + grid.step_until_synchronised}"
 
-puts "--- Day 12: Passage Pathing ---"
-input = File.read_lines("./days/day12.txt")
-caves = CaveSystem.new(input)
-puts "The number of paths through the system is: #{caves.traverse.size}"
-puts "The number of paths through the system where you can visit one small cave more than once is: #{caves.extended_traverse.size}"
+# puts "--- Day 12: Passage Pathing ---"
+# input = File.read_lines("./days/day12.txt")
+# caves = CaveSystem.new(input)
+# puts "The number of paths through the system is: #{caves.traverse.size}"
+# puts "The number of paths through the system where you can visit one small cave more than once is: #{caves.extended_traverse.size}"
 
-puts "--- Day 13: Transparent Origami ---"
-input = File.read("./days/day13.txt")
-code_page = CodePage.new(input)
-code_page.fold
-puts "The number of dots after one fold is: #{code_page.dots.size}"
-code_page.finish_folding
-puts "The code is:\n#{code_page.to_s}"
+# puts "--- Day 13: Transparent Origami ---"
+# input = File.read("./days/day13.txt")
+# code_page = CodePage.new(input)
+# code_page.fold
+# puts "The number of dots after one fold is: #{code_page.dots.size}"
+# code_page.finish_folding
+# puts "The code is:\n#{code_page.to_s}"
 
-puts "--- Day 14: Extended Polymerization ---"
-input = File.read("./days/day14.txt")
-polymer = Polymer.new(input)
-puts "The difference after 10 steps is: #{polymer.step(10)}"
-puts "The difference after 40 steps is: #{polymer.step(40)}"
+# puts "--- Day 14: Extended Polymerization ---"
+# input = File.read("./days/day14.txt")
+# polymer = Polymer.new(input)
+# puts "The difference after 10 steps is: #{polymer.step(10)}"
+# puts "The difference after 40 steps is: #{polymer.step(40)}"
 
-puts "--- Day 15: Chiton ---"
-input = File.read("./days/day15.txt")
-ceil = Ceiling.new(input, 1)
-puts "The lowest risk for the small area is: #{ceil.find_path}"
-ceil = Ceiling.new(input, 5)
-puts "The lowest risk for the large area is: #{ceil.find_path}"
+# puts "--- Day 15: Chiton ---"
+# input = File.read("./days/day15.txt")
+# ceil = Ceiling.new(input, 1)
+# puts "The lowest risk for the small area is: #{ceil.find_path}"
+# ceil = Ceiling.new(input, 5)
+# puts "The lowest risk for the large area is: #{ceil.find_path}"
+
+puts "--- Day 16: Packet Decoder ---"
+input = File.read("./days/day16.txt")
+packets = Packets.parse(input)
+puts Packets.version_total(packets)
+puts packets.map(&.value)
