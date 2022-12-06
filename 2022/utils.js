dc3fc872c35250c3c31f1253aa486a01482d4ee8
Phil Nash <philnash@gmail.com>
2022-12-06 11:58:29 +1100

    Moved data to it's own directory

--- 2022/utils.js
+++ 2022/utils.js
@@ -2,7 +2,7 @@ import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 
 export async function loadData(day) {
-  return await readFile(join("days", `day${day}.txt`), {
+  return await readFile(join("data", `day${day}.txt`), {
     encoding: "utf-8",
   });
 }
